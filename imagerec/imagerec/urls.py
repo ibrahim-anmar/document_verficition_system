088e6f579afc02b05effaf81f569bd50e4181823
Ibrahim Anmar <94973586+ibrahim-anmar@users.noreply.github.com>
2024-05-08 17:37:41 +0300

    Update urls.py add swagger

--- imagerec/imagerec/urls.py
+++ imagerec/imagerec/urls.py
@@ -1,24 +1,14 @@
-"""
-URL configuration for imagerec project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('api_schema',get_schema_view(title='API Schema', description='API for document verification'), name='api_schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='docs.html', 
+        extra_context={'schema_url':'api_schema'}
+        ), name='swagger-ui'),
     path('admin/', admin.site.urls),
     path('', include('imgapp.urls')),
 ]
